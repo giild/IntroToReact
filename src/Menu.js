9a4ebbfa2996f7bd2f498739be06b14850a3526a
giild <peter@giild.com>
2022-02-27 22:51:19 -0500

    changing menu items to anchor tags

--- src/Menu.js
+++ src/Menu.js
@@ -1,12 +1,13 @@
 import * as React from "react";
 
 export const Menu = ({ isMenuOpen }) => {
+
   return (
     <div className={`App-Menu ${isMenuOpen ? "Menu-Open" : ""}`}>
       <div className="Menu-Container">
-      <div className="Menu-Item">Page 1</div>
-      <div className="Menu-Item">Page 2</div>
-      <div className="Menu-Item">Page 3</div>
+        <a className="Menu-Item" name='Page1' href="/">Home</a>
+        <a className="Menu-Item" name='Page2' href="/page1">Page 1</a>
+        <a className="Menu-Item" name='Page3' href="/page2">Page 2</a>
       </div>
     </div>
   );
