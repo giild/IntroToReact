6a21b5f4f9d5ef5fc35b5ee67caaba5dca2bb86e
giild <peter@giild.com>
2022-03-08 15:34:02 -0500

    cleaning things up. I split the two exercises

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import "./App.css";
-import { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
-import {Modal} from "./Modal";
-import {Menu} from "./Menu";
-import { ReactDimmer } from "react-dimmer";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-  const [isModalOpen, setModal] = useState(false);
-  const [isMenuOpen, setMenu] = useState(false);
-
-  const handleMenu = () => {
-    setMenu((prevState) => !prevState);
-  };
-
+function App() {
   return (
-    <>
-      <div className="App">
-        <div className="App-Header">
-          <GiHamburgerMenu className="Menu-Btn" onClick={handleMenu} />
-          <div className="Nav"></div>
-        </div>
-        <div className="body">
-        Example App
-        </div>
-      </div>
-
-      {isModalOpen && <Modal closeModal={setModal} />}
-      <Menu isMenuOpen={isMenuOpen} />
-
-      <ReactDimmer
-        isOpen={isMenuOpen}
-        exitDimmer={setMenu}
-        zIndex={100}
-        blur={1.5}
-      />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hellow World
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
